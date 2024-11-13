86dc0a06e76d79030f71e86a445e8d214630a578
Cosmin-Daniel <cosminsecrier@yahoo.com>
2024-11-13 14:14:21 +0200

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,10 +1,10 @@
 function handleResponse(answer) {
     if (answer === "Yes") {
       // Opens a pop-up window for a "Yes" response
-      window.open("https://example.com/thank-you", "_blank", "width=400,height=200");
+      window.open("https://cocsmin.github.io/AlphaYes/", "_blank", "width=400,height=200");
     } else {
       // Opens a pop-up window for a "No" response
-      window.open("https://example.com/feedback", "_blank", "width=400,height=200");
+      window.open("https://cocsmin.github.io/AlphaNo/", "_blank", "width=400,height=200");
     }
   }
   
