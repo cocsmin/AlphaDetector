c6c994f3c2024c17712ad8dcb6a02c7bb15bcd73
Cosmin-Daniel <cosminsecrier@yahoo.com>
2024-11-13 18:23:22 +0200

    changed a few things, like the music

--- jscript.js
+++ jscript.js
@@ -1,10 +1,12 @@
 function handleResponse(answer) {
     if (answer === "Yes") {
-      // Opens a pop-up window for a "Yes" response
       window.open("https://cocsmin.github.io/AlphaYes/", "_blank", "width=400,height=200");
     } else {
-      // Opens a pop-up window for a "No" response
       window.open("https://cocsmin.github.io/AlphaNo/", "_blank", "width=400,height=200");
     }
   }
   
+document.getElementById("butonda").addEventListener("click", function(){
+    var sound = document.getElementById("auuu");
+    sound.play();
+});
